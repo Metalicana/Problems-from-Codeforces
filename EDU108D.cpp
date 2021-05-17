5f09f922f046e4ac309d4ca6f723bbac0947932c
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-05-16 18:56:48 -0700

    Add files via upload

---
+++ EDU108D.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int,int>
+#define pll pair<long long, long long >
+#define ll long long
+#define lcm(a,b) (a)/__gcd((a),(b))*(b)
+#define pb push_back
+#define sz size()
+#define mp make_pair
+#define rep(i,n) for(i=0;i<(n);i++)
+#define x first
+#define y second
+#define gap ' '
+#define fastio {ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define eps 1e-9
+#define maxn 100000
+#define MOD 1000000007
+#define pi (2.0*acos(0.0))
+#define endl '\n'
+#define trace(x) cerr << #x << ": " << x << endl;
+#define trace2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z) cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+bool equalTo ( double a, double b ){ if ( fabs ( a - b ) <= eps ) return true; else return false; }
+bool notEqual ( double a, double b ){if ( fabs ( a - b ) > eps ) return true; else return false; }
+bool lessThan ( double a, double b ){ if ( a + eps < b ) return true; else return false; }
+bool lessThanEqual ( double a, double b ){if ( a < b + eps ) return true;   else return false;}
+bool greaterThan ( double a, double b ){if ( a > b + eps ) return true;else return false;}
+bool greaterThanEqual ( double a, double b ){if ( a + eps > b ) return true;else return false;}
+bool check(int mask,int i){return ((1<<i)&mask);}
+int Set(int mask,int i){return mask | (1<<i);}
+
+ll add(ll a, ll b)
+{
+  if(a+b<MOD)return a+b;
+  return (a+b)%MOD;
+}
+ll mul(ll a, ll b)
+{
+  if(a*b < MOD)return a*b;
+  return (a*b)%MOD;
+}
+int dir8[2][8] = {{1,-1,0,0,1,-1,1,-1},{0,0,1,-1,1,-1,-1,1}};
+int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+
+//TEMPLATE ENDS HERE, SO DOES MY WILL TO LIVE
+
+int m,n,row,col;
+ll rev[5005][5005];
+ll forw[5005];
+ll a[5005];
+ll b[5005];
+ll sum,mx;
+void solve(int test)
+{
+  scanf("%d",&n);
+  for(int q=0;q<n;q++)scanf("%I64d",&a[q]);
+  for(int q=0;q<n;q++)scanf("%I64d",&b[q]),forw[q]=(q==0?a[q]*b[q]:a[q]*b[q]+forw[q-1]);
+  sum = 0;
+  mx=-4;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=0;j+i-1<n;j++)
+    {
+      if(i==1)rev[j][j+i-1]=a[j]*b[j];
+      else rev[j][j+i-1]=a[j]*b[j+i-1]+a[j+i-1]*b[j] + (i==2?0:rev[j+1][j+i-2]);
+      sum = (j==0?0:forw[j-1]) +rev[j][j+i-1]+ forw[n-1] - forw[j+i-1];
+      mx=max(sum,mx);
+    }
+  }
+  printf("%I64d\n",mx);
+
+
+}
+void test()
+{
+  int t=1;
+  for(int test=1;test<=t;test++)
+  {
+    solve(test);
+  }
+}
+int main()
+{
+  //freopen("o.txt","w",stdout);
+  test();
+  return 0;
+}
